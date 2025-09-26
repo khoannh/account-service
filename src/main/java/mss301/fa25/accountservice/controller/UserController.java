430949738c84982a504150bc144a41940282b4dc
khoannh <nguyenhuukhoan0304@gmail.com>
2025-09-26 20:36:15 +0700

    fix: fix api endpoint

--- src/main/java/mss301/fa25/accountservice/controller/UserController.java
+++ src/main/java/mss301/fa25/accountservice/controller/UserController.java
@@ -10,24 +10,24 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping()
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
-    @PostMapping("/user")
+    @PostMapping()
     public ResponseEntity<User> addUser(@RequestBody User user) {
         userService.addUser(user);
         return ResponseEntity.ok(user);
     }
 
-    @DeleteMapping("/user/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<User> deleteUser(@PathVariable int id) {
         userService.deleteUser(id);
         return ResponseEntity.ok(null);
